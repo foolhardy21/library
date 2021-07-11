96761fe5718ad401e5ccf3df66da85ba2ec6b587
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-07-11 22:34:06 +0530

    6-read-status

--- script.js
+++ script.js
@@ -8,17 +8,20 @@ let library = [
     {
         name: 'Meluha',
         author: 'Amish Tripathi',
-        pages: 300
+        pages: 300,
+        isRead: false
     },
     {
         name: 'Rich Dad Poor Dad',
         author: 'Robert Kiyosaki',
-        pages: 200
+        pages: 200,
+        isRead: false
     },
     {
         name: 'Zarathustra',
         author: 'Friedrich Neitzche',
-        pages: 350
+        pages: 350,
+        isRead: false
     }
 ]
 
@@ -33,12 +36,19 @@ formDiv.addEventListener('submit',(e) => {
 
 })
 
-document.body.addEventListener('click',(e) => {
+libraryDiv.addEventListener('click',(e) => {
 
-    if(e.target.getAttribute('data-id')) {
+    if( e.target.innerText == 'Delete' ) {
         const index = parseInt(e.target.getAttribute('data-id'))
         library.splice(index,1)
 
+        displayLibrary()
+    } else if( e.target.innerText == 'Change' ) {
+        
+        const index = parseInt(e.target.getAttribute('data-id'))
+        let book = new Book(library[index].name,library[index].author,library[index].pages,library[index].isRead)
+        book.changeRead()
+        library[index].isRead = book.isRead
         displayLibrary()
     }
 })
@@ -46,10 +56,15 @@ document.body.addEventListener('click',(e) => {
 
 
 
-function Book(name, author, pages) {
+function Book(name, author, pages, isRead = false) {
     this.name = name
     this.author = author
     this.pages = pages
+    this.isRead = isRead
+}
+Book.prototype.changeRead = function() {
+    this.isRead = !this.isRead
+    
 }
 
 function addBookToLibrary(bookObj) {
@@ -73,19 +88,26 @@ function addBookInDisplay(book,index) {
         let nameDiv = document.createElement('div')
         let authorDiv = document.createElement('div')
         let pageDiv = document.createElement('div')
+        let readDiv = document.createElement('div')
         let removeBtn = document.createElement('button')
-        
+        let readStatusBtn = document.createElement('button')
+
         nameDiv.innerText = book.name
-        authorDiv.innerText = book.author
-        pageDiv.innerText = book.pages
+        authorDiv.innerText = `Written By: ${book.author}`
+        pageDiv.innerText = `No of Pages: ${book.pages}`
+        readDiv.innerText = ` ${book.isRead ? 'Read' : 'Not Read yet'} `
         
         removeBtn.innerText = 'Delete'
         removeBtn.setAttribute('data-id',index)
+        readStatusBtn.innerText = 'Change'
+        readStatusBtn.setAttribute('data-id',index) 
 
         bookDiv.appendChild(nameDiv)
         bookDiv.appendChild(authorDiv)
         bookDiv.appendChild(pageDiv)
+        bookDiv.appendChild(readDiv)
         bookDiv.appendChild(removeBtn)
+        bookDiv.appendChild(readStatusBtn)
         bookDiv.appendChild(document.createElement('hr'))
         bookDiv.appendChild(document.createElement('br'))
 
